e90882a8ffcb72c2b950fff4cf9ceae345ba3a77
Victor Thompson <kelechithompson26@gmail.com>
2023-03-10 12:31:51 +0100

    add prototype assignid

--- js/script.js
+++ js/script.js
@@ -7,3 +7,7 @@ Ticket.prototype.addMovie = function (movie) {
   movie.id = this.assignId();
   this.movies[movie.id] = movie;
 };
+icket.prototype.assignId = function () {
+  this.currentId += 1;
+  return this.currentId;
+};
