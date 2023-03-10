2e970da98ea8032fda676000a89b98619eda46a1
Victor Thompson <kelechithompson26@gmail.com>
2023-03-10 12:31:04 +0100

    add prototype addmovie

--- js/script.js
+++ js/script.js
@@ -3,3 +3,7 @@ function Ticket() {
   this.movies = {};
   this.currentId = 0;
 }
+Ticket.prototype.addMovie = function (movie) {
+  movie.id = this.assignId();
+  this.movies[movie.id] = movie;
+};
